330f360ad66e3af09ba4fa3f94d03e2fef6a47ac
Coranna Howard <me@komiga.com>
2016-01-10 18:46:48 -0500

    test/post/pickle.lua: use custom 404 page.

--- test/post/pickle.lua
+++ test/post/pickle.lua
@@ -10,3 +10,22 @@ local Post = require "src/Post"
 
 -- construct posts over preludes in post/**.html
 P.filter("post", F.match(".*%.html", Post))
+
+local page_data_404 = [[
+<html>
+<head>
+	<title>404 - URL not found</title>
+</head>
+<body>
+
+<h1>404 - URL not found</h1>
+
+<p>Custom 404 page.</p>
+
+</body>
+</html>
+]]
+
+P.filter(function(_, _, _)
+	return P.output(nil, "404.html", page_data_404)
+end)
