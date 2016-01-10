50b74af7703d9e7f7701923c84e360e5fffa802d
Coranna Howard <me@komiga.com>
2016-01-10 08:27:14 -0500

    app/core/build: depend on mmx.
    dep: added mmx.
    dep/.gitignore: exclude mmx.

--- app/core/build.lua
+++ app/core/build.lua
@@ -1,11 +1,20 @@
 
 local S, G, R = precore.helpers()
 
+precore.make_config("pickle.lib.core.mmx.dep", nil, {
+{project = function(p)
+	configuration {}
+		includedirs {
+			G"${DEP_PATH}/mmx/",
+		}
+end}})
+
 precore.make_config("pickle.app.core.dep", {
 	reverse = true,
 }, {
 "pickle.base",
 "togo.lib.core.dep",
+"pickle.lib.core.mmx.dep",
 {project = function(p)
 	pickle.app_config("core")
 end}})
