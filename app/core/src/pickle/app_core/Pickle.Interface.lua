d33dddcf8480bf040781d920898a3e500ecabab1
Coranna Howard <me@komiga.com>
2016-01-13 02:15:11 -0500

    app/core/Pickle.Interface.lua: added font content types.

--- app/core/src/pickle/app_core/Pickle.Interface.lua
+++ app/core/src/pickle/app_core/Pickle.Interface.lua
@@ -201,6 +201,10 @@ do
 	add_content_type("image", "gif")
 	add_content_type("image", "bmp")
 	add_content_type("image", "ico")
+
+	add_content_type("application", "woff", "font-woff")
+	add_content_type("application", "otf", "font-sfnt")
+	add_content_type("application", "ttf", "font-sfnt")
 end
 
 make_command(M.command,
